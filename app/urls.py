d3d4639300e38ed5a41566b2fce8a25836901a92
nur <nurterzi1925@icloud.com>
2024-01-26 12:42:19 +0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import component_list, component_detail, component_edit, component_delete, component_new
+from .views import home, component_list, component_detail, component_edit, component_delete, component_new
 
 urlpatterns = [
+    path('', home, name='home'),
     path('components/', component_list, name='component_list'),
     path('component/<slug:slug>/', component_detail, name='component_detail'),
     path('component/<slug:slug>/edit/', component_edit, name='component_edit'),
