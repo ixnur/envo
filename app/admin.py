f170bee31ea0012c133b93e2d0e2f319f576004e
nur <nurterzi1925@icloud.com>
2024-01-26 18:10:16 +0300

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from .models import Category, Component, ComponentDocumentLink, Document, DocumentType, Location, LocationType, Manufacturer, Package, Purchase, PurchaseDetail, StockMovement
 
-# Register your models here.
 admin.site.register(Category)
 admin.site.register(Component)
-admin.site.register(ComponentDocumentLink)
+
 admin.site.register(Document)
 admin.site.register(DocumentType)
 admin.site.register(Location)
