a2e022d7fb28dff8c16eca99e9067a4efe53baa0
nur <nurterzi1925@icloud.com>
2024-01-29 23:04:25 +0300

    Update settings.py

--- uygulama/settings.py
+++ uygulama/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'app',
     'simple_history',
-    'django.'
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
